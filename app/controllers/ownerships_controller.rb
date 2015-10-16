2b229c1791d4ac31a208710353780e25aa171af7
hasegawa0613 <hasegawa@techfactory.jp>
2015-10-16 10:19:27 +0000

    error modify

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -13,8 +13,7 @@ class OwnershipsController < ApplicationController
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
         response = Amazon::Ecs.item_lookup(params[:asin] , 
-                                  :search_index => 'All' , 
-                                  :response_group => 'Medium' , 
+                                  :response_group => 'Medium', 
                                   :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
