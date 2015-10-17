afdfc19f44fe813ce7d3854d6964c9c4b573394d
hasegawa0613 <hasegawa@techfactory.jp>
2015-10-17 02:53:27 +0000

    ranking error

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,9 @@
 class RankingController < ApplicationController
   def have
-    @items = Item.joins("LEFT OUTER JOIN (SELECT COUNT('ownerships'.'item_id') AS count_item_id, item_id AS item_id FROM 'ownerships' WHERE 'ownerships'.'type' IN ('Have') GROUP BY 'ownerships'.'item_id') rank ON id = rank.item_id").order('count_item_id desc').limit(10)
+    @items = Item.joins("LEFT OUTER JOIN (SELECT COUNT(item_id) AS count_item_id, item_id AS item_id FROM ownerships WHERE type IN ('Have') GROUP BY item_id) rank ON id = rank.item_id").order('count_item_id desc').limit(10)
   end
 
   def want
-    @items = Item.joins("LEFT OUTER JOIN (SELECT COUNT('ownerships'.'item_id') AS count_item_id, item_id AS item_id FROM 'ownerships' WHERE 'ownerships'.'type' IN ('Want') GROUP BY 'ownerships'.'item_id') rank ON id = rank.item_id").order('count_item_id desc').limit(10)
+    @items = Item.joins("LEFT OUTER JOIN (SELECT COUNT(item_id) AS count_item_id, item_id AS item_id FROM ownerships WHERE type IN ('Want') GROUP BY item_id) rank ON id = rank.item_id").order('count_item_id desc').limit(10)
   end
 end
